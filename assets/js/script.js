7b9d05db05a80fa2f6f6b515caf0eeb29160868b
Betül Demir <betuldemir.007@gmail.com>
2022-08-09 12:07:14 +0300

    update
    update js

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+function handleClick(e) {
+    var active = document.querySelector(".active");
+    if(active !==null){
+     active.classList.remove("active");
+    }
+   e.target.className = "active";
+}
+function handleClickk(e) {
+    var active = document.querySelector(".active");
+    if(active !==null){
+     active.classList.remove("active");
+    }
+   e.target.className = "active";
+}
