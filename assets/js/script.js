0485445ef1fd393fc60afbeb2fadb10854b41ae8
Betül Demir <betuldemir.007@gmail.com>
2022-08-09 13:44:28 +0300

    project done
    all updates done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,14 @@
-function handleClick(e) {
-    var active = document.querySelector(".active");
-    if(active !==null){
-     active.classList.remove("active");
+function handle2Click(e) {
+    const h1_active = document.querySelector(".menu--icons .active");
+    if(h1_active !==null){
+        h1_active.classList.remove("active");
     }
    e.target.className = "active";
 }
-function handleClickk(e) {
-    var active = document.querySelector(".active");
-    if(active !==null){
-     active.classList.remove("active");
+function handleClick(e) {
+    const h2_active = document.querySelector(".header-icons .active");
+    if(h2_active !==null){
+        h2_active.classList.remove("active");
     }
    e.target.className = "active";
 }
